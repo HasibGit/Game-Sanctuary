0c9b283b588c3d7f3d788780e44860eb510555a3
HasibGit <hasibullah04@gmail.com>
2023-09-30 14:58:44 +0600

    updated mongodb cluster url

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  //MongoURI : 'mongodb://localhost:27017/gameSanctuaryDB'
-  MongoURI : 'mongodb+srv://admin-hasib:123@gamesanctuary.gik3i.mongodb.net/gameSanctuaryDB'
+  //MongoURI: 'mongodb://localhost:27017/gameSanctuaryDB',
+  //MongoURI : 'mongodb+srv://admin-hasib:123@gamesanctuary.gik3i.mongodb.net/gameSanctuaryDB'
+  MongoURI : 'mongodb+srv://admin-hasib:1qaz<LP_@cluster0.yxsfpws.mongodb.net/gameSanctuaryDB'
 };
